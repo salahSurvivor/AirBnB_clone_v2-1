5bf6f90c848aed54119d6988408a814ed0e68a68
salahSurvivor <salaheddinenaji2000@gmail.com>
2024-02-04 12:24:50 -0800

    try to fix task 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,29 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
-from fabric.api import *
-from datetime import datetime
+"""Fabric script that distributes an archive to your web servers,
+using the function do_deploy."""
+from fabric.api import put, run, env
 from os import path
 
 
 env.hosts = ['54.160.124.170', '100.26.50.248']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
-
+env.key_filename = "secret_key"
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
-
-                # upload archive
-                put(archive_path, '/tmp/')
-
-                # create target dir
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-                # uncompress archive and delete .tgz
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
-
-                # remove archive
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-                # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-                # remove extraneous web_static dir
-                run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-                    .format(timestamp))
-
-                # delete pre-existing sym link
-                run('sudo rm -rf /data/web_static/current')
-
-                # re-establish symbolic link
-                run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-        except:
-                return False
-
-        # return True on success
+    """Function to distribute an archive to your web servers."""
+    if not path.exists(archive_path):
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        name = archive_path.split("/")[-1]
+        name2 = name.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}".format(name2))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(name, name2))
+        run("rm /tmp/{}".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name2, name2))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name2))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name2))
         return True
+    except:
+        return False
